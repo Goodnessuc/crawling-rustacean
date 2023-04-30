96767208a693bf0a1c077184543de1d5903a283b
Goodnessuc <ukejegoodness599@gmail.com>
2023-04-30 08:49:18 +0100

    Finished Tutorial Part

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use reqwest::blocking::get;
+use scraper::{Html, Selector};
+
 fn main() {
-    println!("Hello, world!");
+    let response = get("https://news.ycombinator.com").unwrap().text().unwrap();
+    let html_doc = Html::parse_document(&response);
+    let class_selector = Selector::parse(".titleline").unwrap();
+
+    for element in html_doc.select(&class_selector) {
+        let link_selector = Selector::parse("a").unwrap();
+        for link in element.select(&link_selector) {
+            if let Some(href) = link.value().attr("href") {
+                println!("{}", href);
+            }
+        }
+    }
 }
