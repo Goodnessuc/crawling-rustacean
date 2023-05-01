5919ec84364d4e3da8ee2f8ea72d9ee2bc6b2b45
Goodnessuc <ukejegoodness599@gmail.com>
2023-05-01 16:51:13 +0100

    tutorial completed, project incoming

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,4 @@
 use reqwest::blocking::get;
 use scraper::{Html, Selector};
 
-fn main() {
-    let response = get("https://news.ycombinator.com").unwrap().text().unwrap();
-    let html_doc = Html::parse_document(&response);
-    let class_selector = Selector::parse(".titleline").unwrap();
-
-    for element in html_doc.select(&class_selector) {
-        let link_selector = Selector::parse("a").unwrap();
-        for link in element.select(&link_selector) {
-            if let Some(href) = link.value().attr("href") {
-                println!("{}", href);
-            }
-        }
-    }
-}
+fn main() {}
